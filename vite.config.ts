73ae4569b602d2f419952741eee6f9bd18ce4d16
driikoK <kupchak.a.m@gmail.com>
2026-02-02 19:43:17 +0200

    Update port

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3001,
   },
   plugins: [
     tsconfigPaths(),
